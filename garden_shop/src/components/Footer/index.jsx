98a97b393c691636e46e9e4507dcb4b603a64670
IRZhuravleva <alisa2591@yandex.ru>
2024-03-24 23:03:53 +0100

    Footer without google-map

--- garden_shop/src/components/Footer/index.jsx
+++ garden_shop/src/components/Footer/index.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import s from './index.module.css'
 
 export default function Footer() {
   return (
-    <div>
-        Footer
+    
+    <section className={s.section}>
+      <h2>Contact</h2>
+    <div className={s.contact}>
+      <div className={s.link_card_phone}>
+        <p className={s.small}>Phone</p>
+        <p>+49 999 999 99 99</p>
+
+      </div>
+
+      <div className={s.right_card_socials}>
+       <p className={s.small}>Socials</p>
+       <p></p>
+
+      </div>
+
+      <div className={s.link_card_adress}>
+       <p className={s.small}>Adresse</p>
+       <p>Linkstraße 2, 8 OG, 10785, Berlin, Deutschland</p>
+
+      </div>
+
+      <div className={s.right_card_hours}>
+       <p className={s.small}>Working Hours</p>
+       <p>24 hours a day</p>
+
+      </div>
     </div>
+    
+    </section>
   )
 }
+
