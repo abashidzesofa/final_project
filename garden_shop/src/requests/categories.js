fe294e22438e33b6b20b3d0733ba7b506adbe72b
Sofa Abashidze <abashidzesofaa@gmail.com>
2024-04-22 22:27:43 +0200

    domen changed

--- garden_shop/src/requests/categories.js
+++ garden_shop/src/requests/categories.js
@@ -1,7 +1,7 @@
 import { load_categories_action } from '../store/reducers/categories_reducer'
 
 
-export const domen = 'http://localhost:3333';
+export const domen = 'https://telran-project-backend-c3e9.onrender.com';
 
 
 export const get_categories = dispatch => {
